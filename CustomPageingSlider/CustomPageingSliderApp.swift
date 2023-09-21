fc9d71ac59ae6aa5a36419914dc28fe4ecc78761
githubuiux <uiuxista@gmail.com>
2023-09-21 16:51:23 +0300

    Initial Commit

---
+++ CustomPageingSlider/CustomPageingSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPageingSliderApp.swift
+//  CustomPageingSlider
+//
+//  Created by Levent on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomPageingSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
